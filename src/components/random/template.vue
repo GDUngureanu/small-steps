2ab816679aab5a335694d4f52f0febdff8f4cf73
Georgy Daniel Ungureanu <60849148+GDUngureanu@users.noreply.github.com>
2025-06-04 17:23:59 +0300

    New article, rought implementation

--- src/components/random/template.vue
+++ src/components/random/template.vue
@@ -4,6 +4,57 @@ import articleTemp from '../../templates/article.vue';
 
 <template>
 
+    <articleTemp title="Time Balance - Play the most magnificent game that you can" meta="Jun 4, 2025 by G. D. Ungureanu">
+        <p>
+            Prioritize With Intention: The 3-Bucket Method
+            Divide your energy into 3 life buckets:
+
+            Work / Career
+
+            Relationships (Family, Friends)
+
+            Self (Health, Hobbies, Rest)
+        </p>
+
+
+        <p>The 1-3-5 rule is simple: in order to stay productive at work every day, you just need to commit to
+            accomplishing 1 Major Task, 3 Medium Tasks and 5 Small Tasks.
+
+            <a href="https://www.timedoctor.com/blog/1-3-5-rule/" class="text-decoration-none">What is the 1-3-5
+                Rule?</a>
+            <a href="https://www.developgoodhabits.com/1-3-5-rule/" class="text-decoration-none">How to Use the 1-3-5
+                Rule?</a>
+
+        </p>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <p class="fw-bold">1 Major Task</p> - 2-3 hours
+            </li>
+            <li class="list-group-item">
+                <p class="fw-bold">3 Medium Tasks</p> - 35-60 min
+            </li>
+            <li class="list-group-item">
+                <p class="fw-bold">5 Small Tasks</p> - 10-20 min
+            </li>
+        </ul>
+
+        <p>
+            Choose Hobbies That Energize, Not Deplete
+            You don't need 5 hobbies. Just pick 1-2 that deeply satisfy you and fit your schedule:
+
+            Creative: writing, music, cooking
+
+            Physical: sports, dance, hiking
+
+            Relaxing: reading, gardening, drawing
+
+            Social: games, group classes, clubs
+
+            Even 30 minutes a day is enough to feel restored and connected.
+        </p>
+
+    </articleTemp>
+
     <articleTemp title="Daily Routine" meta="May 27, 2025 by G. D. Ungureanu">
         <p>Taking small steps towards building a healthy life!</p>
 
@@ -62,7 +113,8 @@ import articleTemp from '../../templates/article.vue';
                 <a href="https://vuejs.org/guide/" class="text-decoration-none">Vue.Js</a> - Vue Documentation.
             </li>
             <li class="list-group-item">
-                <a href="https://getbootstrap.com/docs/" class="text-decoration-none">Bootstrap</a> - Bootstrap Documentation.
+                <a href="https://getbootstrap.com/docs/" class="text-decoration-none">Bootstrap</a> - Bootstrap
+                Documentation.
             </li>
             <li class="list-group-item">
                 <a href="https://themewagon.com/" class="text-decoration-none">Theme Wagon</a> - Bootstrap Templates.
