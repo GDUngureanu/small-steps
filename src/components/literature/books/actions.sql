330496d7e8e7f430fbf04ff2961f25e00d01a448
Georgy Daniel Ungureanu <60849148+GDUngureanu@users.noreply.github.com>
2025-08-06 16:41:30 +0300

    Replace book suggestions with actions

---
+++ src/components/literature/books/actions.sql
@@ -0,0 +1,3 @@
+INSERT INTO actions (list_id, description, status, priority) VALUES
+('703c2a54-1678-4e2e-8373-365a8d49f46d','Write an article about Atomic Habits.',false,0),
+('703c2a54-1678-4e2e-8373-365a8d49f46d','Write an article about Ikigai. Don''t forget about the related article https://www.saturdaygift.com/ikigai/.',false,0);
