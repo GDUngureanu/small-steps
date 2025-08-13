db8b5f979f54ed2d5f10de0db3865dc37ad8668b
Georgy Daniel Ungureanu <60849148+GDUngureanu@users.noreply.github.com>
2025-08-13 23:09:51 +0300

    refactor: use constant for budget list id

--- src/pages/finance/Budget.vue
+++ src/pages/finance/Budget.vue
@@ -2,6 +2,8 @@
   import ArticleTemplate from '@/components/shared/templates/Article.vue'
   import ActionsTemplate from '@/components/shared/templates/actions/Actions.vue'
 
+  const LIST_ID = '22222222-2222-2222-2222-222222222222'
+
   defineOptions({
     name: 'BudgetTemplate',
   })
@@ -9,7 +11,7 @@
 
 <template>
   <ArticleTemplate title="Budget Actions" meta="July 31, 2025 by G. D. Ungureanu">
-    <ActionsTemplate list-id="22222222-2222-2222-2222-222222222222" />
+    <ActionsTemplate :list-id="LIST_ID" />
   </ArticleTemplate>
 </template>
 
