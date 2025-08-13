68b9efe7ebcd3990c80b1a6c68ae8c7cee91562e
Georgy Daniel Ungureanu <ungureanu.georgy@gmail.com>
2025-08-13 15:54:11 +0300

    Remove reduntdant data.

--- src/pages/entertainment/actions.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO actions (list_id, description, status, priority) VALUES
-('e0ad76c7-28ab-4d43-80f9-0c463979226c','Add watched Anime.',false,0),
-('e0ad76c7-28ab-4d43-80f9-0c463979226c','Add images for Anime.',false,0),
-('e0ad76c7-28ab-4d43-80f9-0c463979226c','Add Icons for Airing Status, View Status and Rating.',false,0),
-('e0ad76c7-28ab-4d43-80f9-0c463979226c','Add possibility of filter table and automatically sort based on Rating.',false,0),
-('e0ad76c7-28ab-4d43-80f9-0c463979226c','Add rankings from MyAnimeList.net.',false,0),
-('e0ad76c7-28ab-4d43-80f9-0c463979226c','Consider adding pagination to the list.',false,0);
