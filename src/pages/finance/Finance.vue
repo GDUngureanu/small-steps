fba674cc197833d7af2252ace9fd14eaa9ab9166
Georgy Daniel Ungureanu <60849148+GDUngureanu@users.noreply.github.com>
2025-08-13 23:11:02 +0300

    refactor: use constant for finance list id

--- src/pages/finance/Finance.vue
+++ src/pages/finance/Finance.vue
@@ -5,11 +5,13 @@
   defineOptions({
     name: 'FinanceTemplate',
   })
+
+  const LIST_ID = '11111111-1111-1111-1111-111111111111'
 </script>
 
 <template>
   <ArticleTemplate title="Finance Actions" meta="July 31, 2025 by G. D. Ungureanu">
-    <ActionsTemplate list-id="11111111-1111-1111-1111-111111111111" />
+    <ActionsTemplate :list-id="LIST_ID" />
   </ArticleTemplate>
 </template>
 
