9258e65912ed978c1c68e76beea8ded885b9e72b
Georgy Daniel Ungureanu <60849148+GDUngureanu@users.noreply.github.com>
2025-08-13 23:10:13 +0300

    refactor: use constant for investments list id

--- src/pages/finance/Investments.vue
+++ src/pages/finance/Investments.vue
@@ -2,6 +2,8 @@
   import ArticleTemplate from '@/components/shared/templates/Article.vue'
   import ActionsTemplate from '@/components/shared/templates/actions/Actions.vue'
 
+  const LIST_ID = '33333333-3333-3333-3333-333333333333'
+
   defineOptions({
     name: 'InvestmentsTemplate',
   })
@@ -9,7 +11,7 @@
 
 <template>
   <ArticleTemplate title="Investment Actions" meta="July 31, 2025 by G. D. Ungureanu">
-    <ActionsTemplate list-id="33333333-3333-3333-3333-333333333333" />
+    <ActionsTemplate :list-id="LIST_ID" />
   </ArticleTemplate>
 </template>
 
