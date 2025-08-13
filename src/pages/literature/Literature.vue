b23fed6be538b2fb2a442293a5ba315dcb718b3f
Georgy Daniel Ungureanu <60849148+GDUngureanu@users.noreply.github.com>
2025-08-14 00:50:52 +0300

    refactor(literature): use constant list id

--- src/pages/literature/Literature.vue
+++ src/pages/literature/Literature.vue
@@ -5,10 +5,12 @@
   defineOptions({
     name: 'LiteratureTemplate',
   })
+
+  const LIST_ID = '7a171dc3-50bd-4732-b912-08f25fcba7a6'
 </script>
 
 <template>
   <ArticleTemplate title="Literature Actions" meta="July 31, 2025 by G. D. Ungureanu">
-    <ActionsTemplate list-id="7a171dc3-50bd-4732-b912-08f25fcba7a6" />
+    <ActionsTemplate :list-id="LIST_ID" />
   </ArticleTemplate>
 </template>
